0064d443487236c1d7d60fc5fd9e3b3600cab9ea
Rishab <ra.rishab@gmail.com>
2013-11-14 17:22:29 +0530

    Switched to TwentyFourteen. Starting from scratch.

--- footer.php
+++ footer.php
@@ -1,25 +1,29 @@
 <?php
 /**
  * The template for displaying the footer
- * Modified from TwentyTwelve
  *
  * Contains footer content and the closing of the #main and #page div elements.
  *
  * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
  */
 ?>
-	</div><!-- #main .wrapper -->
-	<footer id="colophon" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'twentytwelve_credits' ); ?>
-			This website is powered by <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( '%s', 'twentytwelve' ), 'WordPress.' ); ?></a>
-Theme by <a href="https://github.com/SocietyforKnowledgeCommons/knowledge-commons-wp" title="Knowledge Commons Theme">Society for Knowledge Commons</a>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
 
-<?php wp_footer(); ?>
+		</div><!-- #main -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+
+			<?php get_sidebar( 'footer' ); ?>
+
+			<div class="site-info">
+				<?php do_action( 'twentyfourteen_credits' ); ?>
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s. ', 'twentyfourteen' ), 'WordPress' ); ?></a>
+				Theme by <a href="https://github.com/SocietyforKnowledgeCommons/knowledge-commons-wp" title="Knowledge Commons Theme">Society for Knowledge Commons</a>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
 </body>
 </html>
