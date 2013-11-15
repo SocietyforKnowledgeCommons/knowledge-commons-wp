e7a9bafd1e1877a228cb9c0f2a5fc0de9dedf404
Rishab <ra.rishab@gmail.com>
2013-11-15 14:32:56 +0530

    Added list of child page bookmarks

--- page-templates/scroll.php
+++ page-templates/scroll.php
@@ -18,6 +18,26 @@ get_header(); ?>
 			<?php
                 $parent_id = $post->ID;
                 $pages = get_pages( array('child_of' =>  $parent_id) );
+
+                echo "<ul id=\"single-page-nav-bar\">";
+                    foreach ($pages as $page_data) {
+                        echo "<li class='single-page-nav-bar-item'><a href='#$page_data->post_name'>";
+                        echo $page_data->post_title;
+                        echo "</a></li>\n";
+                    }
+                echo "</ul>";
+
+
+                echo "<article class='page type-page status-publish hentry no-post-thumbnail'>\n";
+                echo "<header class=\"entry-header\"><h1 class='entry-title'>";
+                echo $post->post_title;
+                echo "</h1></header>\n";
+                echo "<div class='entry-content'>";
+                echo $post->post_content;
+                echo "</div>\n";
+                echo "</article>\n";
+
+
 				foreach ($pages as $page_data) {
 					$title = $page_data->post_title;
 					$slug = $page_data->post_name;
@@ -26,8 +46,12 @@ get_header(); ?>
                     $title = $page_data->post_title;
 
                     echo "<div class='scroll-page' id='$pageid'><a name='$slug'></a>";
-                    echo "<h2>$title</h2>";
+                    echo "<article class='page type-page status-publish hentry no-post-thumbnail'>";
+                    echo "<header class=\"entry-header\"><h1 class='entry-title'>$title</h1></header>";
+                    echo "<div class='entry-content'>";
                     echo $content;
+                    echo "</div>";
+                    echo "</article>";
                     echo "</div>";
                 }
 
