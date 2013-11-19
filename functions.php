9a5f038f6a490dc0acbb38a52708d5c87589fd36
Rishab <ra.rishab@gmail.com>
2013-11-19 16:21:58 +0530

    |#31|RA|Added icon upload facility in wp-admin

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Knowledge Commons functions and definitions
+ *
+ * @package WordPress
+ * @subpackage Knowledge Commons
+ * @since Knowledge Commons 1.0
+ */
+
+
+function knowledgecommons_theme_customizer( $wp_customize ) {
+    $wp_customize->add_section( 'knowledgecommons_logo_section' , array(
+        'title'       => __( 'Logo', 'knowledgecommons' ),
+        'priority'    => 30,
+        'description' => 'Upload a logo to display beside the title',
+    ) );
+    
+    $wp_customize->add_setting( 'knowledgecommons_logo' );
+
+    $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'knowledgecommons_logo', array(
+        'label'    => __( 'Logo', 'knowledgecommons' ),
+        'section'  => 'knowledgecommons_logo_section',
+        'settings' => 'knowledgecommons_logo',
+    ) ) );
+}
+add_action('customize_register', 'knowledgecommons_theme_customizer');
+?>
