85046756a4902c8324c782b75ef6d0bc2bd9e8ad
Rishab <ra.rishab@gmail.com>
2013-11-25 13:02:15 +0530

    qTranslate patch: To enable events plugin with qTranslate

--- functions.php
+++ functions.php
@@ -27,4 +27,29 @@ add_action('customize_register', 'knowledgecommons_theme_customizer');
 
 add_action( 'update_option_theme_mods_knowledge-commons-wp', 'twentyfourteen_rebuild_accent_colors' );
 
+
+
+// qTranslate hack
+
+// hook
+
+add_action('em_event', em_qtranslate, 0, 3);
+
+// qtranslate
+
+function em_qtranslate($target, $arg1=null, $arg2=null, $arg3=null) {
+  $target->event_name = em_qtranslate_string($target->event_name);
+  $target->event_owner = em_qtranslate_string($target->event_owner);
+  $target->post_content = em_qtranslate_string($target->post_content);
+}
+
+function em_qtranslate_string($raw_string) {
+  if(function_exists('qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage'))
+    $output = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($raw_string);
+  else
+    $output = __($raw_string);
+  return $output;
+}
+
+
 ?>
